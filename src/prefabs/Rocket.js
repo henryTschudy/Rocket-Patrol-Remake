caaa4e4cb931c87a079e7f862f75f50191fc1093
henryTschudy <htschudy@ucsc.edu>
2021-04-09 14:01:11 -0700

    Update to current class state

---
+++ src/prefabs/Rocket.js
@@ -0,0 +1,31 @@
+class Rocket extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame) {
+        super(scene, x, y, texture, frame);
+        scene.add.existing(this);
+        this.movementSpeed = 2;
+        this.isFiring = false;
+    }
+
+    update() {
+        if(this.isFiring) {
+
+        } else {
+            if(keyLEFT.isDown) {
+                this.x -= this.movementSpeed;
+            }
+            if(keyRIGHT.isDown) {
+                this.x += this.movementSpeed;
+            }
+    
+            if(Phaser.Input.Keyboard.JustDown(keyF)) {
+                this.isFiring = true;
+            }
+
+            this.x = Phaser.Math.Clamp(
+                this.x,
+                borderUISize + borderPadding,
+                game.config.width - borderUISize - borderPadding);
+        }
+        
+    }
+}
