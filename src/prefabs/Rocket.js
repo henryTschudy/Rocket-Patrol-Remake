0c3acc4ad1ac85016a130826d0e19c017b324b03
henryTschudy <htschudy@ucsc.edu>
2021-04-09 14:24:29 -0700

    Added rocket collision

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -2,7 +2,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame) {
         super(scene, x, y, texture, frame);
         scene.add.existing(this);
-        this.movementSpeed = 2;
+        this.movementSpeed = 3;
         this.isFiring = false;
     }
 
@@ -10,8 +10,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
         if(this.isFiring) {
             this.y -= this.movementSpeed;
             if(this.y < borderUISize*3) {
-                this.y = game.config.height-borderUISize-borderPadding;
-                this.isFiring = false;
+                this.reset();
             }
         } else {
             if(keyLEFT.isDown) {
@@ -20,16 +19,21 @@ class Rocket extends Phaser.GameObjects.Sprite {
             if(keyRIGHT.isDown) {
                 this.x += this.movementSpeed;
             }
-    
+
             if(Phaser.Input.Keyboard.JustDown(keyF)) {
                 this.isFiring = true;
             }
-
+    
             this.x = Phaser.Math.Clamp(
                 this.x,
                 borderUISize + borderPadding,
                 game.config.width - borderUISize - borderPadding);
         }
         
+    }
+
+    reset() {
+        this.y = game.config.height-borderUISize-borderPadding;
+        this.isFiring = false;
     }
 }
